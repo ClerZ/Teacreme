54959190d025d84241b165d82a34b1e1fbf7641e
ClerZ <150576610+ClerZ@users.noreply.github.com>
2023-12-12 15:01:13 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,117 @@
+const wintermellonButtons1 = document.querySelectorAll('.bs1-size1');
+const wintermellonButtons2 = document.querySelectorAll('.bs1-size2');
+const okinawa1 = document.querySelectorAll('.bs2-size1');
+const okinawa2 = document.querySelectorAll('.bs2-size2');
+const oreocremecheese1 = document.querySelectorAll('.bs3-size1');
+const oreocremecheese2 = document.querySelectorAll('.bs3-size2');
+
+wintermellonButtons1.forEach(function(btn) {
+    btn.addEventListener("click", function() {
+        console.log("Wintermellon-16oz clicked");
+
+        // Reset background color for all buttons
+        wintermellonButtons1,wintermellonButtons2.forEach(function(innerBtn) {
+            innerBtn.classList.remove('selected');
+            innerBtn.style.backgroundColor = 'white';
+            innerBtn.style.color = '#15154E';
+        });
+
+        // Add 'selected' class and change background color to the clicked button
+        btn.classList.add('selected');
+        btn.style.backgroundColor = '#15154E';
+        btn.style.color = 'white';    });
+});
+
+wintermellonButtons2.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+      console.log("Wintermellon-22oz clicked");
+
+      // Reset background color for all buttons
+      wintermellonButtons2,wintermellonButtons1.forEach(function(innerBtn) {
+          innerBtn.classList.remove('selected');
+          innerBtn.style.backgroundColor = 'white';
+          innerBtn.style.color = '#15154E';
+      });
+
+      // Add 'selected' class and change background color to the clicked button
+      btn.classList.add('selected');
+      btn.style.backgroundColor = '#15154E';
+      btn.style.color = 'white';
+  });
+});
+
+okinawa1.forEach(function(btn) {
+    btn.addEventListener("click", function() {
+        console.log("Okinawa-16oz clicked");
+
+        // Reset background color for all buttons
+        okinawa1,okinawa2.forEach(function(innerBtn) {
+            innerBtn.classList.remove('selected');
+            innerBtn.style.backgroundColor = 'white';
+            innerBtn.style.color = '#15154E';
+        });
+
+        // Add 'selected' class and change background color to the clicked button
+        btn.classList.add('selected');
+        btn.style.backgroundColor = '#15154E';
+        btn.style.color = 'white';
+    });
+});
+
+okinawa2.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+      console.log("Okinawa-22oz clicked");
+
+      // Reset background color for all buttons
+      okinawa2,okinawa1.forEach(function(innerBtn) {
+          innerBtn.classList.remove('selected');
+          innerBtn.style.backgroundColor = 'white';
+          innerBtn.style.color = '#15154E';
+      });
+
+      // Add 'selected' class and change background color to the clicked button
+      btn.classList.add('selected');
+      btn.style.backgroundColor = '#15154E';
+      btn.style.color = 'white';
+  });
+});
+
+oreocremecheese1.forEach(function(btn) {
+    btn.addEventListener("click", function() {
+        console.log("Oreo Cremecheese-16oz clicked");
+
+        // Reset background color for all buttons
+        oreocremecheese1,oreocremecheese2.forEach(function(innerBtn) {
+            innerBtn.classList.remove('selected');
+            innerBtn.style.backgroundColor = 'white';
+            innerBtn.style.color = '#15154E';
+        });
+
+        // Add 'selected' class and change background color to the clicked button
+        btn.classList.add('selected');
+        btn.style.backgroundColor = '#15154E';
+        btn.style.color = 'white';    
+      });
+});
+
+oreocremecheese2.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+      console.log("Oreo Cremecheese-22oz clicked");
+
+      // Reset background color for all buttons
+      oreocremecheese2,oreocremecheese1.forEach(function(innerBtn) {
+          innerBtn.classList.remove('selected');
+          innerBtn.style.backgroundColor = 'white';
+          innerBtn.style.color = '#15154E';
+    });
+
+      // Add 'selected' class and change background color to the clicked button
+      btn.classList.add('selected');
+      btn.style.backgroundColor = '#15154E';
+      btn.style.color = 'white';  });
+});
+
+
 document.addEventListener('DOMContentLoaded', function () {
   // Add smooth scrolling to links with the "scroll-link" class
   var scrollLinks = document.querySelectorAll('.scroll-link');
@@ -15,3 +129,28 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+function performSearch() {
+  var searchTerm = document.getElementById('searchInput').value.trim().toLowerCase();
+
+  // Check if the search term is not empty
+  if (searchTerm !== '') {
+    var flavorContainers = document.querySelectorAll('.txtformat');
+
+    // Remove existing highlights
+    var elements = document.querySelectorAll('.highlight');
+    elements.forEach(function (element) {
+      element.classList.remove('highlight');
+    });
+
+    flavorContainers.forEach(function (container) {
+      var flavorText = container.textContent.toLowerCase();
+
+      if (flavorText.includes(searchTerm)) {
+        container.classList.add('highlight');
+
+        container.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      }
+    });
+  }
+}
